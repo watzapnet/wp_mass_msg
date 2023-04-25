9f7a1ff3fa20dc0c21a7cf8b21ea910b13e8e25c
watzapnet <97122398+watzapnet@users.noreply.github.com>
2023-04-25 21:54:16 +0300

    Added Output feature
    Added output html file
    
    - Total message sent
    - Total time

--- wp.py
+++ wp.py
@@ -1,9 +1,12 @@
+# -*- coding: utf-8 -*-
+
 import json
 import requests
 import time
+from datetime import timedelta
 
 # Whatsapp API endpoint
-url = 'http://Whatsapp-Api-URL'
+url = 'http://WP-API-URL'
 
 # Add your api key
 auth_key = 'Bearer $2b$10$NTCAqVeMGRyuEPxOecyxVOo8La1iSLvwtcK7D4CnE1g4NSKDpAMLu'
@@ -21,6 +24,15 @@ with open(message_file, 'r') as f:
 # Read the numbers from file
 with open(phones_file, 'r') as f:
     phones = [line.strip() for line in f.readlines()]
+
+# Start time
+start_time = time.monotonic()
+
+# Success count
+success_count = 0
+
+# Failure count
+failure_count = 0
 
 # Api request for each number
 for phone in phones:
@@ -40,8 +52,39 @@ for phone in phones:
     # If unsuccess, provide error response
     if response.status_code != 200:
         print(f'Hata kodu: {response.status_code} - Hata mesajı: {response.text}')
+        failure_count += 1
     else:
         print(f'{phone} numarasına mesaj gönderildi.')
+        success_count += 1
 
     # wait 10 secs between each msg
     time.sleep(10)
+
+# End time
+end_time = time.monotonic()
+
+# Elapsed time
+elapsed_time = end_time - start_time
+
+# Total count
+total_count = success_count + failure_count
+
+# Create HTML output
+html_output = f"""
+<html>
+    <body>
+        <h2>Whatsapp Mesaj Gönderme Raporu</h2>
+        <p>Toplam gönderilen mesaj sayısı: {total_count}</p>
+        <p>Toplam başarılı mesaj sayısı: {success_count}</p>
+        <p>Toplam başarısız mesaj sayısı: {failure_count}</p>
+        <p>Toplam geçen süre: {str(timedelta(seconds=elapsed_time))}</p>
+        <p>Mesaj gönderimi tamamlandı.</p>
+    </body>
+</html>
+"""
+
+# Save HTML output to file
+with open('output.html', 'w') as f:
+    f.write(html_output)
+
+print("Rapor başarıyla oluşturuldu ve 'output.html' dosyasına kaydedildi.")
